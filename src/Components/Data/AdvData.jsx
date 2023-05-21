45324c03bec63f80e26939245769476e24ffdb9a
Hasibul Hasan <hasibulhasan442@gmail.com>
2023-05-21 23:29:39 +0600

    Featured Products Added

---
+++ src/Components/Data/AdvData.jsx
@@ -0,0 +1,32 @@
+export default [
+    {
+      id: 1,
+      image: "/image/background/Product-Adv-1.jpg",
+      title_en: "20% Off On Tank Tops",
+      title_pt: "20% de desconto em regatas",
+      description_en:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      description_pt:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac dictum.",
+    },
+    {
+      id: 2,
+      image: "/image/background/Product-Adv-2.jpeg",
+      title_en: "Latest Eyewear For You",
+      title_pt: "Últimos óculos para você",
+      description_en:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      description_pt:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac dictum.",
+    },
+    {
+      id: 3,
+      image: "/image/background/Product-Adv-3.jpg",
+      title_en: "Let's Lorem Suit Up!",
+      title_pt: "Vamos Lorem Suit Up!",
+      description_en:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      description_pt:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac dictum.",
+    },
+  ];
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,43 +5,11 @@ import ClientSlider from "../../Components/Clientslider/ClientSlider";
 import AdvCard from "../../Components/Card/AdvCard";
 import ProductCard from "../../Components/Card/ProductCard";
 import ProductData from "../../Components/Data/ProductData";
+import AdvData from "../../Components/Data/AdvData";
 function Home() {
   const { t } = useTranslation();
-  const adv_data = [
-    {
-      id: 1,
-      image: "/image/background/Product-Adv-1.jpg",
-      title_en: "20% Off On Tank Tops",
-      title_pt: "20% de desconto em regatas",
-      description_en:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      description_pt:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac dictum.",
-    },
-    {
-      id: 2,
-      image: "/image/background/Product-Adv-2.jpeg",
-      title_en: "Latest Eyewear For You",
-      title_pt: "Últimos óculos para você",
-      description_en:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      description_pt:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac dictum.",
-    },
-    {
-      id: 3,
-      image: "/image/background/Product-Adv-3.jpg",
-      title_en: "Let's Lorem Suit Up!",
-      title_pt: "Vamos Lorem Suit Up!",
-      description_en:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      description_pt:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac dictum.",
-    },
-  ];
-
+  const adv_data = AdvData;
   const Products = ProductData;
-  console.log(Products);
   return (
     <>
       <section className="banner-body">
