2a3c9a07f6760e5eaaba5da515d8326ff9348134
Hasibul Hasan <hasibulhasan442@gmail.com>
2023-05-25 08:36:32 +0000

    Footer Error Fixed

--- src/Include/Footer.jsx
+++ src/Include/Footer.jsx
@@ -5,92 +5,146 @@ function Footer() {
     <>
       <section className="footerBlock">
         <div className="container">
-          
-        <footer className="text-white text-center text-lg-start" style="background-color: #23242a;">
+          <footer
+            className="text-white text-center text-lg-start"
+            style={{backgroundColor: "#23242a"}}
+          >
+            <div className="container p-4">
+              <div className="row mt-4">
+                <div className="col-lg-4 col-md-12 mb-4 mb-md-0">
+                  <h5 className="text-uppercase mb-4">About company</h5>
 
-    <div className="container p-4">
+                  <p>
+                    At vero eos et accusamus et iusto odio dignissimos ducimus
+                    qui blanditiis praesentium voluptatum deleniti atque
+                    corrupti.
+                  </p>
 
-      <div className="row mt-4">
+                  <p>
+                    Blanditiis praesentium voluptatum deleniti atque corrupti
+                    quos dolores et quas molestias.
+                  </p>
 
-        <div className="col-lg-4 col-md-12 mb-4 mb-md-0">
-          <h5 className="text-uppercase mb-4">About company</h5>
+                  <div className="mt-4">
+                    <a
+                      type="button"
+                      className="btn btn-floating btn-warning btn-lg"
+                    >
+                      <i className="fab fa-facebook-f"></i>
+                    </a>
 
-          <p>
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-            voluptatum deleniti atque corrupti.
-          </p>
+                    <a
+                      type="button"
+                      className="btn btn-floating btn-warning btn-lg"
+                    >
+                      <i className="fab fa-dribbble"></i>
+                    </a>
 
-          <p>
-            Blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas
-            molestias.
-          </p>
+                    <a
+                      type="button"
+                      className="btn btn-floating btn-warning btn-lg"
+                    >
+                      <i className="fab fa-twitter"></i>
+                    </a>
 
-          <div className="mt-4">
+                    <a
+                      type="button"
+                      className="btn btn-floating btn-warning btn-lg"
+                    >
+                      <i className="fab fa-google-plus-g"></i>
+                    </a>
+                  </div>
+                </div>
 
-            <a type="button" className="btn btn-floating btn-warning btn-lg"><i className="fab fa-facebook-f"></i></a>
+                <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
+                  <h5 className="text-uppercase mb-4 pb-1">Search something</h5>
 
-            <a type="button" className="btn btn-floating btn-warning btn-lg"><i className="fab fa-dribbble"></i></a>
+                  <div className="form-outline form-white mb-4">
+                    <input
+                      type="text"
+                      id="formControlLg"
+                      className="form-control form-control-lg"
+                    />
+                    <label
+                      className="form-label"
+                      for="formControlLg"
+                      style={{marginLeft: "0px"}}
+                    >
+                      Search
+                    </label>
+                    <div className="form-notch">
+                      <div
+                        className="form-notch-leading"
+                        style={{width: "9px"}}
+                      ></div>
+                      <div
+                        className="form-notch-middle"
+                        style={{width: "48.8px"}}
+                      ></div>
+                      <div className="form-notch-trailing"></div>
+                    </div>
+                  </div>
 
-            <a type="button" className="btn btn-floating btn-warning btn-lg"><i className="fab fa-twitter"></i></a>
+                  <ul className="fa-ul" style={{marginLeft: "1.65em"}}>
+                    <li className="mb-3">
+                      <span className="fa-li">
+                        <i className="fas fa-home"></i>
+                      </span>
+                      <span className="ms-2">New York, NY 10012, US</span>
+                    </li>
+                    <li className="mb-3">
+                      <span className="fa-li">
+                        <i className="fas fa-envelope"></i>
+                      </span>
+                      <span className="ms-2">info@example.com</span>
+                    </li>
+                    <li className="mb-3">
+                      <span className="fa-li">
+                        <i className="fas fa-phone"></i>
+                      </span>
+                      <span className="ms-2">+ 01 234 567 88</span>
+                    </li>
+                    <li className="mb-3">
+                      <span className="fa-li">
+                        <i className="fas fa-print"></i>
+                      </span>
+                      <span className="ms-2">+ 01 234 567 89</span>
+                    </li>
+                  </ul>
+                </div>
 
-            <a type="button" className="btn btn-floating btn-warning btn-lg"><i className="fab fa-google-plus-g"></i></a>
+                <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
+                  <h5 className="text-uppercase mb-4">Opening hours</h5>
 
-          </div>
-        </div>
-
-        <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
-          <h5 className="text-uppercase mb-4 pb-1">Search something</h5>
-
-          <div className="form-outline form-white mb-4">
-            <input type="text" id="formControlLg" className="form-control form-control-lg" />
-            <label className="form-label" for="formControlLg" style="margin-left: 0px;">Search</label>
-          <div className="form-notch"><div className="form-notch-leading" style="width: 9px;"></div><div className="form-notch-middle" style="width: 48.8px;"></div><div className="form-notch-trailing"></div></div></div>
-
-          <ul className="fa-ul" style="margin-left: 1.65em;">
-            <li className="mb-3">
-              <span className="fa-li"><i className="fas fa-home"></i></span><span className="ms-2">New York, NY 10012, US</span>
-            </li>
-            <li className="mb-3">
-              <span className="fa-li"><i className="fas fa-envelope"></i></span><span className="ms-2">info@example.com</span>
-            </li>
-            <li className="mb-3">
-              <span className="fa-li"><i className="fas fa-phone"></i></span><span className="ms-2">+ 01 234 567 88</span>
-            </li>
-            <li className="mb-3">
-              <span className="fa-li"><i className="fas fa-print"></i></span><span className="ms-2">+ 01 234 567 89</span>
-            </li>
-          </ul>
-        </div>
-
-        <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
-          <h5 className="text-uppercase mb-4">Opening hours</h5>
-
-          <table className="table text-center text-white">
-            <tbody className="font-weight-normal">
-              <tr>
-                <td>Mon - Thu:</td>
-                <td>8am - 9pm</td>
-              </tr>
-              <tr>
-                <td>Fri - Sat:</td>
-                <td>8am - 1am</td>
-              </tr>
-              <tr>
-                <td>Sunday:</td>
-                <td>9am - 10pm</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-
-      </div>
-    </div>
-    <div className="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-      © 2020 Copyright:
-      <a className="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-    </div>
-  </footer>
-
+                  <table className="table text-center text-white">
+                    <tbody className="font-weight-normal">
+                      <tr>
+                        <td>Mon - Thu:</td>
+                        <td>8am - 9pm</td>
+                      </tr>
+                      <tr>
+                        <td>Fri - Sat:</td>
+                        <td>8am - 1am</td>
+                      </tr>
+                      <tr>
+                        <td>Sunday:</td>
+                        <td>9am - 10pm</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+            <div
+              className="text-center p-3"
+              style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}
+            >
+              © 2023 Copyright:
+              <a className="text-white" href="#">
+                Hasibul Hasan
+              </a>
+            </div>
+          </footer>
         </div>
       </section>
     </>
